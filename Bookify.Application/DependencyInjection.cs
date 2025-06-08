bde11f180085e34cfded3110d34e4dea959a6d4f
farooq-teqniqly <farooq@teqniqly.com>
2025-06-08 08:43:33 -0700

    chore: Configure DI and MediatR.

---
+++ Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Bookings;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<IPricingService, PricingService>();
+
+            return services;
+        }
+    }
+}
